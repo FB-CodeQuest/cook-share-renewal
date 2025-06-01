d715c79f7866de179605e49ffaeec611db6a9251
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2025-06-01 23:27:13 +0900

    feat: create SignupStep03 screen

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,9 +1,10 @@
 import SignupStep01 from "@/pages/Signup/SignupStep01.jsx";
 import SignupStep02 from "@/pages/Signup/SignupStep02.jsx";
+import SignupStep03 from "@/pages/Signup/SignupStep03.jsx";
 
 const Signup = () => {
     return(
-        <SignupStep02 />
+        <SignupStep03 />
     )
 }
 export default Signup;
