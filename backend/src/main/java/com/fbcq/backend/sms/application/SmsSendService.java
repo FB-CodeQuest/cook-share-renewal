14acdf89f4a7d59c423590e797908a3d6f7b3d62
김강민 <sskiee@naver.com>
2025-06-03 00:01:31 +0900

    feat: add exception for INVALID_AUTH_CODE, AUTH_CODE_ALREADY_SENT

--- backend/src/main/java/com/fbcq/backend/sms/application/SmsSendService.java
+++ backend/src/main/java/com/fbcq/backend/sms/application/SmsSendService.java
@@ -2,11 +2,14 @@ package com.fbcq.backend.sms.application;
 
 // SmsSendService.java
 
+import com.fbcq.backend.global.exception.AuthCodeResendNotAllowedException;
 import com.fbcq.backend.sms.domain.AuthCodeGenerator;
 import com.fbcq.backend.sms.domain.AuthCodeStore;
 import com.fbcq.backend.sms.domain.SmsMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class SmsSendService {
     private final AuthCodeGenerator codeGenerator;
@@ -16,6 +19,9 @@ public class SmsSendService {
     private static final int AUTH_CODE_TTL_SECONDS = 300;
 
     public void sendAuthCode(String phoneNumber) {
+        if (authCodeStore.get(phoneNumber).isPresent()) {
+            throw new AuthCodeResendNotAllowedException(); // 3분 내 재전송 불가
+        }
         String code = codeGenerator.generate();
         SmsMessage message = SmsMessage.authCode(phoneNumber, code);
 
